9a46d2720cfa3bd6c843381491aed9ecaead3ffd
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 01:07:06 +0200

    config(tailwind): extend palette with discord color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "smoke": "#f2f3f5",
+        "active": "#5865f2",
+        "active-extra": "#23a559",
+        "server-browser": "#1e1f22",
+        "server-browser-divider": "#35363c",
+        "server-browser-icon": "#dbdee1",
+        "server-sidebar": "#2b2d31",
+        "server-chat": "#313338",
+        input: "#383a40",
+        "input-icon": "#b5bac1",
+      },
+    },
   },
   plugins: [],
 };
