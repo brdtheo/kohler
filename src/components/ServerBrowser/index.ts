9ef29f05181acf7dca71695af8ffaf981e3f330d
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 02:20:10 +0200

    feat(server-browser): create server browser component

---
+++ src/components/ServerBrowser/index.ts
@@ -0,0 +1,16 @@
+import ServerBrowser from "./ServerBrowser.component";
+import ServerBrowserButton, {
+  Props as ServerBrowserButtonProps,
+} from "./ServerBrowserButton";
+import ServerBrowserDivider from "./ServerBrowserDivider";
+import ServerBrowserList from "./ServerBrowserList";
+import ServerBrowserListItem from "./ServerBrowserListItem";
+
+export {
+  ServerBrowserButton,
+  ServerBrowserDivider,
+  ServerBrowserList,
+  ServerBrowserListItem,
+};
+export type { ServerBrowserButtonProps };
+export default ServerBrowser;
