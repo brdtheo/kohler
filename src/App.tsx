6b981475d4169abdbfdf7f9ff5d05d4d2075df74
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 14:51:52 +0200

    feat(types): update User and Server types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import ServerBrowser from "@components/ServerBrowser";
 
-import { UserRegisteredServer } from "@/types/common";
+import { Server } from "@/types/server";
 
-const _TEMP_SERVER_LIST: UserRegisteredServer[] = [
+const _TEMP_SERVER_LIST: Server[] = [
   {
     id: 1,
     name: "TailwindCSS",
     thumbnail:
       "https://yt3.googleusercontent.com/ikv41jMTr1uHGdILrJhvbfVJcDt4oqhwApKX37TjAleF_cRPbF2W-waj7uMnS5JySvnlvAlTCg=s900-c-k-c0x00ffffff-no-rj",
+    created_at: "",
+    invite_code: "",
   },
 ];
 
