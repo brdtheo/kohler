39ac5391c9ea870daca9afabd7f37853141d40eb
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 18:30:10 +0200

    feat(hooks): add appbar icons in useIcon hook

--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -1,6 +1,14 @@
 enum IconName {
   LOGO = "logo",
   ADD = "add",
+  HASHTAG = "hashtag",
+  HASHTAG_CHAT_BUBBLE = "hashtagChatBubble",
+  NOTIFICATION = "notification",
+  PIN = "pin",
+  MEMBERS = "members",
+  SEARCH = "search",
+  INBOX = "inbox",
+  HELP = "help",
 }
 
 export { IconName };
