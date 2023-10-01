3536a8510f3c0d77575c05721e45b8351bd0698f
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 02:21:12 +0200

    feat(server-browser): update default route and add temp data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import ServerBrowser from "@components/ServerBrowser";
+
+import { UserRegisteredServer } from "@/types/common";
+
+const _TEMP_SERVER_LIST: UserRegisteredServer[] = [
+  {
+    id: 1,
+    name: "TailwindCSS",
+    thumbnail:
+      "https://yt3.googleusercontent.com/ikv41jMTr1uHGdILrJhvbfVJcDt4oqhwApKX37TjAleF_cRPbF2W-waj7uMnS5JySvnlvAlTCg=s900-c-k-c0x00ffffff-no-rj",
+  },
+];
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen text-xl">
-      Kohler
+    <div className="grid grid-cols-auto w-full h-screen bg-server-chat">
+      <ServerBrowser serverList={_TEMP_SERVER_LIST} />
     </div>
   );
 }
