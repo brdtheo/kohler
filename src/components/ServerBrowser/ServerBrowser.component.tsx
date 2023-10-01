6bd83c556f9aa452a3f056041483ea14ae341f2d
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 02:20:10 +0200

    feat(server-browser): create server browser component

---
+++ src/components/ServerBrowser/ServerBrowser.component.tsx
@@ -0,0 +1,45 @@
+import Icon from "@components/Icon";
+import {
+  ServerBrowserDivider,
+  ServerBrowserList,
+  ServerBrowserListItem,
+} from "./";
+
+import { type UserRegisteredServer, IconName } from "@/types/common";
+
+type Props = {
+  serverList: UserRegisteredServer[];
+};
+
+const ServerBrowser: React.FC<Props> = ({ serverList }) => (
+  <div className="bg-server-browser w-[72px] h-full flex flex-col gap-3">
+    <ServerBrowserList>
+      <>
+        <ServerBrowserListItem title="Direct Messages">
+          <Icon name={IconName.LOGO} />
+        </ServerBrowserListItem>
+
+        {serverList && (
+          <>
+            <ServerBrowserDivider />
+            {serverList.map((server) => (
+              <ServerBrowserListItem
+                key={server.id}
+                title={server.name}
+                thumbnail={server.thumbnail}
+              />
+            ))}
+          </>
+        )}
+
+        <ServerBrowserDivider />
+
+        <ServerBrowserListItem title="Add a server" isExtraAction>
+          <Icon name={IconName.ADD} />
+        </ServerBrowserListItem>
+      </>
+    </ServerBrowserList>
+  </div>
+);
+
+export default ServerBrowser;
