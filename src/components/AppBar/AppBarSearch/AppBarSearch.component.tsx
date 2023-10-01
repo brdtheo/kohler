0963d1671588360862733292c1ff611be104010d
Théo Billardey <toto.billardey@gmail.com>
2023-10-01 20:16:24 +0200

    feat(app-bar): create search component

---
+++ src/components/AppBar/AppBarSearch/AppBarSearch.component.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useState } from "react";
+
+import Icon from "@components/Icon";
+
+import { IconName } from "@/types/common";
+
+type Props = {
+  value: string;
+  onSearch: () => void;
+};
+
+const AppBarSearch: React.FC<Props> = ({ value }) => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleOnFocus = useCallback(() => setIsInputFocused(true), []);
+  const handleOnBlur = useCallback(() => setIsInputFocused(false), []);
+
+  return (
+    <div
+      className={`transition-all ease-in duration-200 rounded bg-server-browser overflow-hidden mx-2 h-6 ${
+        isInputFocused ? "w-60" : "w-36"
+      }`}
+    >
+      <div className="relative">
+        <input
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
+          className="bg-inherit outline-none h-6 text-sm gg-medium text-smoke pl-2 w-full pr-6"
+          placeholder="Search"
+        />
+        {value ? (
+          <Icon
+            name={IconName.SEARCH}
+            className={`w-6 h-6 p-1 box-border absolute right-0 top-0 ${
+              value ? "text-icon" : "text-server-browser-icon"
+            }`}
+          />
+        ) : (
+          <Icon
+            name={IconName.SEARCH}
+            className={`w-6 h-6 p-1 box-border absolute right-0 top-0 ${
+              value ? "text-icon" : "text-server-browser-icon"
+            }`}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AppBarSearch;
