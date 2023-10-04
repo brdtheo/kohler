c28b1057792070bbe473f7331d69a4306c58ca33
Théo Billardey <toto.billardey@gmail.com>
2023-10-04 21:12:39 +0200

    feat(hooks): create useTooltip hook

---
+++ src/hooks/useTooltip.tsx
@@ -0,0 +1,94 @@
+import { useRef } from "react";
+
+import {
+  FloatingArrow,
+  Placement,
+  arrow,
+  autoUpdate,
+  offset,
+  useFloating,
+  useHover,
+  useInteractions,
+} from "@floating-ui/react";
+
+/**
+ * Returns all of the required props for tooltip used in server browser button
+ *
+ * More infos in the docs: https://floating-ui.com/docs/tooltip#usefloating-hook
+ *
+ * @example
+ * const { refs, tooltipNode, getReferenceProps } = useTooltip({
+ *  placement: "right",
+ *  open: showTooltip,
+ *  content: server.title,
+ *  onOpenChange: setShowTooltip,
+ * });
+ */
+export const useTooltip = ({
+  placement,
+  content,
+  open,
+  mainAxisOffset,
+  onOpenChange,
+}: {
+  /** Where the child element should be positioned */
+  placement: Placement;
+  /** Text to be displayed within the tooltip */
+  content: string;
+  /** Boolean that controls the display of the tooltip */
+  open: boolean;
+  /** Main axis offset in px */
+  mainAxisOffset?: number;
+  /** Set state action to change the boolean */
+  onOpenChange: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const arrowRef = useRef(null);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open,
+    placement,
+    onOpenChange,
+    middleware: [
+      offset({
+        mainAxis: mainAxisOffset,
+      }),
+      arrow({
+        element: arrowRef,
+      }),
+    ],
+    whileElementsMounted: autoUpdate,
+  });
+
+  const hover = useHover(context, { move: false });
+
+  // Merge all the interactions into prop getters
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
+  const tooltipNode = (
+    <div
+      className="w-max flex items-center px-3 rounded h-10 bg-contextual text-sm font-medium text-smoke relative gg-semibold"
+      ref={refs.setFloating}
+      style={floatingStyles}
+      {...getFloatingProps()}
+    >
+      <FloatingArrow
+        ref={arrowRef}
+        context={context}
+        className="fill-contextual"
+        width={10}
+        height={4}
+      />
+      {content}
+    </div>
+  );
+
+  return {
+    arrowRef,
+    refs,
+    floatingStyles,
+    context,
+    tooltipNode,
+    getReferenceProps,
+    getFloatingProps,
+  };
+};
