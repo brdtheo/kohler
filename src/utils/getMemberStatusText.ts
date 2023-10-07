01b2555ee2635f9ce5551677544040abc4907774
Théo Billardey <toto.billardey@gmail.com>
2023-10-08 01:32:36 +0200

    feat(utils): create member status text util

---
+++ src/utils/getMemberStatusText.ts
@@ -0,0 +1,22 @@
+import { UserStatus } from "@/types/user";
+
+/**
+ * Retrieve the tooltip text from a Member status
+ * @example
+ * const { tooltipNode } = useTooltip({
+ *   // ...
+ *   content: getMemberStatusText(status) ?? "",
+ *   // ...
+ * });
+ */
+export default function getMemberStatusText(status?: UserStatus) {
+  if (!status) return "";
+  switch (status) {
+    case UserStatus.ONLINE:
+      return "Online";
+    case UserStatus.IDLE:
+      return "Idle";
+    case UserStatus.DO_NOT_DISTURB:
+      return "Do Not Disturb";
+  }
+}
