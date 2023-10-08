2bf4e43b897b383e11eb47718a7f48ad9abaae45
Théo Billardey <toto.billardey@gmail.com>
2023-10-08 16:26:55 +0200

    feat(members-list): create server members list component

---
+++ src/components/ServerMembersList/ServerMembersList.components.tsx
@@ -0,0 +1,61 @@
+import { useMemo } from "react";
+
+import ServerMemberListItem from "./ServerMemberListItem";
+
+import { UserStatus } from "@/types/user";
+import { ServerMember } from "@/types/server";
+
+type Props = {
+  isOpen: boolean;
+  members: ServerMember[];
+};
+
+const ServerMembersList: React.FC<Props> = ({ isOpen, members }) => {
+  const onlineMembers = useMemo(
+    () => members.filter((member) => member.status !== UserStatus.OFFLINE),
+    [members]
+  );
+
+  const offlineMembers = useMemo(
+    () => members.filter((member) => member.status === UserStatus.OFFLINE),
+    [members]
+  );
+
+  return (
+    isOpen && (
+      <aside className="bg-caviar w-60 h-full">
+        <div className="overflow-y-auto pb-5 pr-2 h-full scrollbar-thin">
+          <h3 className="pt-6 pr-2 pl-4 uppercase gg-semibold text-xs text-crestline">
+            online — {onlineMembers.length}
+          </h3>
+          <ul className="ml-2">
+            {onlineMembers.map((member) => (
+              <ServerMemberListItem
+                key={member.id}
+                memberStatus={member.status}
+                memberName={member.username}
+                avatarThumbnail={member.thumbnail}
+              />
+            ))}
+          </ul>
+
+          <h3 className="pt-6 pr-2 pl-4 uppercase gg-semibold text-xs text-crestline">
+            offline — {offlineMembers.length}
+          </h3>
+          <ul className="ml-2">
+            {offlineMembers.map((member) => (
+              <ServerMemberListItem
+                key={member.id}
+                memberStatus={UserStatus.OFFLINE}
+                memberName={member.username}
+                avatarThumbnail={member.thumbnail}
+              />
+            ))}
+          </ul>
+        </div>
+      </aside>
+    )
+  );
+};
+
+export default ServerMembersList;
