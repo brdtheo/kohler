5bea0844e88fca049fbebca3b340cd701e88e177
Théo Billardey <toto.billardey@gmail.com>
2023-10-08 16:26:55 +0200

    feat(members-list): create server members list component

---
+++ src/components/ServerMembersList/index.ts
@@ -0,0 +1,5 @@
+import ServerMembersList from "./ServerMembersList.components";
+import ServerMemberListItem from "./ServerMemberListItem";
+
+export { ServerMemberListItem };
+export default ServerMembersList;
