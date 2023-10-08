3ec5685ddbad699510118f681074510b5daef312
Théo Billardey <toto.billardey@gmail.com>
2023-10-08 14:56:57 +0200

    feat(types): create user status enum and update user fields

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,3 +1,10 @@
+enum UserStatus {
+  OFFLINE = 0,
+  ONLINE = 1,
+  IDLE = 2,
+  DO_NOT_DISTURB = 3,
+}
+
 type User = {
   /** User ID */
   id: number;
@@ -5,8 +12,15 @@ type User = {
   email: string;
   /** Username associated with this User (can be replaced by `display_name` in Server) */
   username: string;
+  /** The current status of the User. Can also be set manually */
+  status: UserStatus;
+  /** An optional avatar picture */
+  thumbnail?: string;
+  /** An optional profile description. Will partially be shown in the server members list  */
+  bio?: string;
   /** Date when the user signed up to Kohler */
   created_at: string;
 };
 
+export { UserStatus };
 export type { User };
