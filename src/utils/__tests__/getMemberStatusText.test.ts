904657a040b9df8f5bd9a2d32bf8a59db1dfe316
Théo Billardey <toto.billardey@gmail.com>
2023-10-22 02:39:37 +0200

    build(jest): create test files for utils

---
+++ src/utils/__tests__/getMemberStatusText.test.ts
@@ -0,0 +1,24 @@
+import { test, describe, expect } from '@jest/globals';
+
+import getMemberStatusText from '@utils/getMemberStatusText';
+
+import { UserStatus } from '@/types/user';
+
+describe('[hook]: getMemberStatusText', () => {
+  test('is returning an empty string if status not provided', () => {
+    const result = getMemberStatusText(undefined);
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual('');
+  });
+
+  test('is returning the correct text associated to the user status', () => {
+    const onlineStatusResult = getMemberStatusText(UserStatus.ONLINE);
+    const idleStatusResult = getMemberStatusText(UserStatus.IDLE);
+    const doNotDisturbStatusResult = getMemberStatusText(
+      UserStatus.DO_NOT_DISTURB,
+    );
+    expect(onlineStatusResult).toStrictEqual('Online');
+    expect(idleStatusResult).toStrictEqual('Idle');
+    expect(doNotDisturbStatusResult).toStrictEqual('Do Not Disturb');
+  });
+});
