7d04bea67626d4af1df7d203fcbee6b636c38193
Théo Billardey <toto.billardey@gmail.com>
2023-10-22 02:39:37 +0200

    build(jest): create test files for utils

---
+++ src/utils/__tests__/getBoardEmojiBackgroundPosition.test.ts
@@ -0,0 +1,19 @@
+import { test, describe, expect } from '@jest/globals';
+import { faker } from '@faker-js/faker';
+
+import getBoardEmojiBackgroundPosition from '@utils/getBoardEmojiBackgroundPosition';
+
+describe('[hook]: getBoardEmojiBackgroundPosition', () => {
+  test('is returning a correct background position', () => {
+    const maxCols = faker.number.int({ min: 1, max: 10 });
+    const maxRows = faker.number.int({ min: 1, max: 10 });
+    const emojiSize = faker.number.int({ min: 1, max: 10 });
+    const result = getBoardEmojiBackgroundPosition(maxCols, maxRows, emojiSize);
+    expect(result).toBeTruthy();
+  });
+
+  test('is returning a zero based background position when zero in params', () => {
+    const result = getBoardEmojiBackgroundPosition(0, 0, 0);
+    expect(result).toStrictEqual(`-0px -0px`);
+  });
+});
---
+++ src/utils/__tests__/getMemberStatusColor.test.ts
@@ -0,0 +1,24 @@
+import { test, describe, expect } from '@jest/globals';
+
+import getMemberStatusColor from '@utils/getMemberStatusColor';
+
+import { UserStatus } from '@/types/user';
+
+describe('[hook]: getMemberStatusColor', () => {
+  test('is returning an empty string if status not provided', () => {
+    const result = getMemberStatusColor(undefined);
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual('');
+  });
+
+  test('is returning the correct background color associated to the user status', () => {
+    const onlineStatusResult = getMemberStatusColor(UserStatus.ONLINE);
+    const idleStatusResult = getMemberStatusColor(UserStatus.IDLE);
+    const doNotDisturbStatusResult = getMemberStatusColor(
+      UserStatus.DO_NOT_DISTURB,
+    );
+    expect(onlineStatusResult).toStrictEqual('bg-grass');
+    expect(idleStatusResult).toStrictEqual('bg-cheesy');
+    expect(doNotDisturbStatusResult).toStrictEqual('bg-flamingo');
+  });
+});
