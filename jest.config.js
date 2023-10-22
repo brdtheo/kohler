3e7009abd9e3f761481204d79160568b8a2a4789
Théo Billardey <toto.billardey@gmail.com>
2023-10-22 02:37:54 +0200

    config(jest): create config file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@/types/(.*)$': '<rootDir>/src/types/$1',
+  },
+};
