39b3834256d3b0323483bd699de0ef7a05a43d0a
Théo Billardey <toto.billardey@gmail.com>
2023-10-28 15:09:33 +0200

    feat(message): import related components and temp data

--- src/libs/server/ServerActivity/ServerActivity.component.tsx
+++ src/libs/server/ServerActivity/ServerActivity.component.tsx
@@ -1,15 +1,24 @@
-import ServerActivityMessageInput from '../../message/MessageInput';
+import MessageList from '@libs/message/MessageList';
+import MessageInput from '@libs/message/MessageInput';
+
+import { SentMessage } from '@/types/message';
 
 type Props = {
   /** The name of the channel. Used for message input placeholder */
   channelName: string;
+  /** The messages list passed for rendering */
+  messagesList: SentMessage[];
+  onSendMessage: (message: SentMessage) => void;
 };
 
-const ServerActivity: React.FC<Props> = ({ channelName }) => (
+const ServerActivity: React.FC<Props> = ({
+  channelName,
+  messagesList,
+  onSendMessage,
+}) => (
   <div className="flex flex-1 bg-ebony flex-col">
-    <div className="flex-1" />
-
-    <ServerActivityMessageInput channelName={channelName} />
+    <MessageList messagesList={messagesList} />
+    <MessageInput channelName={channelName} onSendMessage={onSendMessage} />
   </div>
 );
 
