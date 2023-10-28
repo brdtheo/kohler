a3fb3847c76482a1cf814540da7c66ac0103a43d
Théo Billardey <toto.billardey@gmail.com>
2023-10-28 14:55:55 +0200

    feat(message): create message component

---
+++ src/libs/message/Message/Message.component.tsx
@@ -0,0 +1,38 @@
+import Avatar from '@components/Avatar';
+import useMessageDateTime from '@hooks/useMesageTime';
+
+import type { SentMessage } from '@/types/message';
+import { AvatarSize } from '@/types/common';
+
+type Props = {
+  message: SentMessage;
+};
+
+const Message: React.FC<Props> = ({ message }) => {
+  const messageSentDate = useMessageDateTime(message.sent_at);
+
+  return (
+    <li className="w-full relative">
+      <div className="pl-18 pr-12 py-1 mt-3.5 flex relative hover:bg-charcoal">
+        <button className="absolute top-1 left-4 cursor-pointer">
+          <Avatar thumbnail={message.thumbnail} size={AvatarSize.LARGE} />
+        </button>
+
+        <div>
+          <div className="flex items-center">
+            <h3 className="text-smoke gg-medium leading-[22px] mr-1 cursor-pointer hover:underline">
+              {message.username}
+            </h3>
+            <span className="text-placeholder text-xs ml-1 cursor-default">
+              {messageSentDate}
+            </span>
+          </div>
+
+          <span className="text-iron gg-regular">{message.content}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default Message;
