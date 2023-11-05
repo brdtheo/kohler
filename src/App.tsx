242689192ce1fdad07c5b2100626dc4c5da635dd
Théo Billardey <toto.billardey@gmail.com>
2023-11-05 19:05:17 +0100

    feat(sidebar): import component and update temp data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useCallback, useState } from 'react';
 import dayjs from 'dayjs';
+import { faker } from '@faker-js/faker';
 
 import ServerBrowser from '@libs/server/ServerBrowser';
 import AppBar from '@components/AppBar';
 import ServerMembersList from '@libs/member/MembersList';
 import ServerActivity from '@libs/server/ServerActivity';
+import Sidebar from '@components/Sidebar';
 
 import {
   Server,
@@ -13,16 +15,30 @@ import {
   ServerMember,
 } from '@/types/server';
 import { MessageType, SentMessage } from '@/types/message';
-import { UserStatus } from '@/types/user';
-import { faker } from '@faker-js/faker';
+import { User, UserStatus } from '@/types/user';
 
 const _TEMP_SERVER_LIST: Server[] = [
   {
     id: 1,
-    name: 'CS2',
+    name: 'Counter-Strike 2',
     thumbnail: 'https://i.redd.it/jfr58wvzl5db1.jpg',
-    created_at: '',
-    invite_code: '',
+    created_at: dayjs(faker.date.recent()).toISOString(),
+    invite_code: faker.string.alphanumeric({ length: 6 }),
+  },
+  {
+    id: 2,
+    name: 'Wumpus and friends',
+    thumbnail: 'https://i.redd.it/pptk3al4l0j71.jpg',
+    created_at: dayjs(faker.date.recent()).toISOString(),
+    invite_code: faker.string.alphanumeric({ length: 6 }),
+  },
+  {
+    id: 3,
+    name: 'Duolingo',
+    thumbnail:
+      'https://static.wixstatic.com/media/24bb9b_bac9c76ca749476bb314752df0970ac2~mv2.png/v1/fill/w_1000,h_1000,al_c,q_90,usm_0.66_1.00_0.01/24bb9b_bac9c76ca749476bb314752df0970ac2~mv2.png',
+    created_at: dayjs(faker.date.recent()).toISOString(),
+    invite_code: faker.string.alphanumeric({ length: 6 }),
   },
 ];
 
@@ -64,6 +80,17 @@ const _TEMP_MESSAGES_LIST: SentMessage[] = new Array(20).fill(0).map(() => ({
   thumbnail: faker.internet.avatar(),
 }));
 
+const _TEMP_USER: User = {
+  id: faker.number.int(1000),
+  email: faker.internet.email(),
+  username: 'brdtheo',
+  status: UserStatus.DO_NOT_DISTURB,
+  thumbnail:
+    'https://cdn.discordapp.com/avatars/338044684423397376/3af412869d429758cb9782b7789c8d06.webp',
+  bio: undefined,
+  created_at: faker.date.recent().toDateString(),
+};
+
 export default function App() {
   const [isMembersListOpen, setIsMembersListOpen] = useState(true);
   const [messages, setMessages] = useState<SentMessage[]>(_TEMP_MESSAGES_LIST);
@@ -79,6 +106,9 @@ export default function App() {
   return (
     <div className="flex w-full h-screen">
       <ServerBrowser serverList={_TEMP_SERVER_LIST} />
+
+      <Sidebar userStatus={_TEMP_USER.status} userName={_TEMP_USER.username} />
+
       <div className="flex-1 bg-gray-700 overflow-hidden">
         <AppBar
           title={_TEMP_SERVER_CHANNEL.name}
