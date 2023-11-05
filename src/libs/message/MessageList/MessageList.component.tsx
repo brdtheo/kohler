f7f21ced87afcc835e666e145d720835dfb253f3
Théo Billardey <toto.billardey@gmail.com>
2023-11-05 18:13:59 +0100

    fix(message): set message list auto scroll
    Resolves #15

--- src/libs/message/MessageList/MessageList.component.tsx
+++ src/libs/message/MessageList/MessageList.component.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import Message from '@libs/message/Message';
 
 import type { SentMessage } from '@/types/message';
@@ -8,9 +10,24 @@ type Props = {
 };
 
 const MessageList: React.FC<Props> = ({ messagesList }) => {
+  const listRef = useRef<HTMLOListElement | null>(null);
+  const listContentRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (listRef?.current && listContentRef?.current) {
+      listRef.current.scrollTo(0, listContentRef.current.scrollHeight);
+    }
+  }, [messagesList.length]);
+
   return (
-    <ol className="flex-1 overflow-y-scroll scrollbar-large border-r-4 border-transparent">
-      <div className="message-list-height flex flex-col justify-end">
+    <ol
+      className="flex-1 overflow-y-scroll scrollbar-large border-r-4 border-transparent"
+      ref={listRef}
+    >
+      <div
+        className="message-list-height flex flex-col justify-end"
+        ref={listContentRef}
+      >
         {messagesList.map((message) => (
           <Message key={message.id} message={message} />
         ))}
