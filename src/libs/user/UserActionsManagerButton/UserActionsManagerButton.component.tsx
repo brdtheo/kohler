1664bde120ca26a12c5d8724eabe718758affad1
Théo Billardey <toto.billardey@gmail.com>
2023-11-11 00:45:09 +0100

    feat(user-actions-manager): add buttons tooltip

--- src/libs/user/UserActionsManagerButton/UserActionsManagerButton.component.tsx
+++ src/libs/user/UserActionsManagerButton/UserActionsManagerButton.component.tsx
@@ -1,23 +1,49 @@
+import { useState } from 'react';
+
 import Icon from '@components/Icon';
+import { useTooltip } from '@hooks/useTooltip';
 
 import { IconName } from '@/types/common';
 
 type Props = {
   /** The name of the icon element displayed in the button */
   iconName: IconName;
+  /** The text displayed in the tooltip on button hover */
+  tooltipText: string;
   /** The button click handler */
   onClick: () => void;
 };
 
-const UserActionsManagerButton: React.FC<Props> = ({ iconName, onClick }) => {
+const UserActionsManagerButton: React.FC<Props> = ({
+  iconName,
+  tooltipText,
+  onClick,
+}) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const { refs, tooltipNode, getReferenceProps } = useTooltip({
+    placement: 'top',
+    open: showTooltip,
+    content: tooltipText,
+    mainAxisOffset: 8,
+    maxWidth: 160,
+    onOpenChange: setShowTooltip,
+  });
+
   return (
-    <button
-      className="text-crestline w-8 h-8 flex justify-center items-center hover:bg-smoke hover:text-smoke hover:text-opacity-90 hover:bg-opacity-10 rounded"
-      type="button"
-      onClick={onClick}
-    >
-      <Icon name={iconName} />
-    </button>
+    <>
+      <button
+        className="text-crestline w-8 h-8 flex justify-center items-center hover:bg-smoke hover:text-smoke hover:text-opacity-90 hover:bg-opacity-10 rounded"
+        type="button"
+        onClick={onClick}
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
+        <Icon name={iconName} />
+      </button>
+
+      {showTooltip && <span className="text-center">{tooltipNode}</span>}
+    </>
   );
 };
 
