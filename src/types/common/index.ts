da72b001e278167f663654d00bf62b63bee3aa06
Théo Billardey <toto.billardey@gmail.com>
2023-11-11 18:02:07 +0100

    refactor(icons): remove unused hook and enum
    Resolve #4

--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -1,32 +1,6 @@
-enum IconName {
-  LOGO = 'logo',
-  ADD = 'add',
-  HASHTAG = 'hashtag',
-  HASHTAG_CHAT_BUBBLE = 'hashtagChatBubble',
-  NOTIFICATION = 'notification',
-  PIN = 'pin',
-  MEMBERS = 'members',
-  SEARCH = 'search',
-  INBOX = 'inbox',
-  HELP = 'help',
-  ATTACH = 'attach',
-  GIFT = 'gift',
-  GIF = 'gif',
-  STICKER = 'sticker',
-  TYPING_DOTS = 'typingDots',
-  MICROPHONE = 'microphone',
-  MICROPHONE_MUTED = 'microphone_muted',
-  HEADSET = 'headset',
-  HEADSET_DEAFEN = 'headset_deafen',
-  SETTINGS = 'settings',
-  DISCONNECT = 'disconnect',
-  AUDIO_WAVE = 'audio_wave',
-  NETWORK = 'network',
-}
-
 enum AvatarSize {
   SMALL = 'small',
   LARGE = 'large',
 }
 
-export { IconName, AvatarSize };
+export { AvatarSize };
