0647e2ce4e60994d47eb299ea332f892d754436a
Théo Billardey <toto.billardey@gmail.com>
2023-11-19 16:33:36 +0100

    feat(login): import form in page

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,10 +1,34 @@
-import { Link } from 'react-router-dom';
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router';
+
+import CharacterBackground from '@components/CharacterBackground';
+import LoginForm from '@components/LoginForm';
 
 const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleForgotPasswordClick = useCallback(() => {}, []);
+
+  const handleLogin = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setIsSubmit(true);
+      setTimeout(() => {
+        navigate('/channels/@me');
+      }, 1000);
+    },
+    [navigate],
+  );
+
   return (
-    <div>
-      <p>Login</p>
-      <Link to="/channels/@me">LOGIN</Link>
+    <div className="w-screen h-screen flex justify-center items-center select-none gg-regular">
+      <CharacterBackground />
+      <LoginForm
+        isLoading={isSubmit}
+        onForgotPasswordClick={handleForgotPasswordClick}
+        onSubmit={handleLogin}
+      />
     </div>
   );
 };
