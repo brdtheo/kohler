ef19091bdaa62a6aae1e623bef0e600501cc1609
Théo Billardey <toto.billardey@gmail.com>
2023-11-21 22:59:31 +0100

    feat(channel-browser): create list component

---
+++ src/libs/channel/ChannelList/ChannelList.component.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from 'react';
+
+import ChannelListCategory from '@libs/channel/ChannelCategory';
+
+import { ChannelType, ServerChannel } from '@/types/server';
+
+type Props = {
+  serverId: number;
+  serverChannels: ServerChannel[];
+  selectedChannel: number;
+};
+
+const ChannelList: React.FC<Props> = ({
+  serverId,
+  serverChannels,
+  selectedChannel,
+}) => {
+  const textChannels = useMemo(
+    () => serverChannels.filter((channel) => channel.type === ChannelType.TEXT),
+    [serverChannels],
+  );
+
+  const audioChannels = useMemo(
+    () =>
+      serverChannels.filter((channel) => channel.type === ChannelType.AUDIO),
+    [serverChannels],
+  );
+
+  return (
+    <ul className="relative">
+      <ChannelListCategory
+        categoryName="text channels"
+        categoryChannels={textChannels}
+        selectedChannel={selectedChannel}
+        serverId={serverId}
+      />
+
+      <ChannelListCategory
+        categoryName="voice channels"
+        categoryChannels={audioChannels}
+        selectedChannel={selectedChannel}
+        serverId={serverId}
+      />
+    </ul>
+  );
+};
+
+export default ChannelList;
