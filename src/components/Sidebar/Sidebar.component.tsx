d9b3e218a79fe8d165b465984c89c26d0a51b62a
Théo Billardey <toto.billardey@gmail.com>
2023-11-21 23:03:45 +0100

    feat(channel-browser): import related components

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -1,27 +1,47 @@
+import SidebarHeader from '@components/SidebarHeader';
+
+import ChannelBrowser from '@libs/channel/ChannelBrowser/ChannelBrowser.component';
 import UserActionsManager from '@libs/user/UserActionsManager';
 
+import { ServerChannel } from '@/types/server';
 import { UserStatus } from '@/types/user';
 
 type Props = {
   /** The name of the server displayed in the sidebar header and RTC state */
   serverName: string;
+  /** The current server id. Used to navigate in channels */
+  serverId: number;
+  /** Available server channels */
+  serverChannels: ServerChannel[];
   /** The user status; used to display the color within the avatar */
   userStatus: UserStatus;
   /** User name displayed next to the avatar */
   userName: string;
   /** The name of the channel, displayed when active RTC */
   channelName: string;
+  selectedChannel: number;
 };
 
 const Sidebar: React.FC<Props> = ({
   userStatus,
   userName,
   serverName,
+  serverId,
+  serverChannels,
   channelName,
+  selectedChannel,
 }) => {
+  console.log(location);
+
   return (
-    <div className="bg-caviar w-60 flex flex-col">
-      <div className="flex-1 text-smoke" />
+    <nav className="bg-caviar w-60 flex flex-col">
+      <SidebarHeader serverName={serverName} />
+
+      <ChannelBrowser
+        serverId={serverId}
+        serverChannels={serverChannels}
+        selectedChannel={selectedChannel}
+      />
 
       <UserActionsManager
         userStatus={userStatus}
@@ -29,7 +49,7 @@ const Sidebar: React.FC<Props> = ({
         serverName={serverName}
         channelName={channelName}
       />
-    </div>
+    </nav>
   );
 };
 
