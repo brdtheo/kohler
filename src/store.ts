cd7b3221426e30c9637064bac605057594866fab
Théo Billardey <toto.billardey@gmail.com>
2023-12-26 15:13:07 +0100

    feat(redux): configure store and import provider

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { serverApi } from '@libs/server/api';
+import serverSlice from '@libs/server/serverSlice';
+
+export const store = configureStore({
+  reducer: {
+    [serverApi.reducerPath]: serverApi.reducer,
+    server: serverSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(serverApi.middleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
