3974b186c78eed18e1ba3857872b3d0ffa15d143
Théo Billardey <toto.billardey@gmail.com>
2023-12-26 15:13:07 +0100

    feat(redux): configure store and import provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import routes from '@routes';
+
+import { store } from '@/store';
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
   </React.StrictMode>,
 );
 
