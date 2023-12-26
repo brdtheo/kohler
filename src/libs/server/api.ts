1f8481d00831644c99b9786d8514dcacb4cd2f9d
Théo Billardey <toto.billardey@gmail.com>
2023-12-26 15:12:15 +0100

    feat(redux): create server api

---
+++ src/libs/server/api.ts
@@ -0,0 +1,44 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query';
+import { gql } from 'graphql-request';
+
+import getApiRequestHeaders from '@utils/getApiRequestHeaders';
+import getCleanGraphQLResponse from '@utils/getCleanGraphQLResponse';
+
+import { BaseApiResponse } from '@/types/common';
+import { Server } from '@/types/server';
+
+// Define a service using a base URL and expected endpoints
+export const serverApi = createApi({
+  reducerPath: 'serverApi',
+  baseQuery: graphqlRequestBaseQuery({
+    url: import.meta.env.VITE_BASE_API_URL,
+    prepareHeaders: (headers) => getApiRequestHeaders(headers),
+  }),
+  endpoints: (builder) => ({
+    getServerList: builder.query<Server[], void>({
+      query: () => ({
+        document: gql`
+          query getServerList($page: Int = 1, $per_page: Int = 30) {
+            serversCollection {
+              edges {
+                node {
+                  id
+                  name
+                  thumbnail
+                  invite_code
+                }
+              }
+            }
+          }
+        `,
+      }),
+      transformResponse: (response: BaseApiResponse<Server>) =>
+        getCleanGraphQLResponse(response),
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetServerListQuery } = serverApi;
--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -1,6 +1,17 @@
+type GraphNode<T> = {
+  node: T;
+};
+
+type BaseApiResponse<T> = {
+  [key: string]: {
+    edges: GraphNode<T>[];
+  };
+};
+
 enum AvatarSize {
   SMALL = 'small',
   LARGE = 'large',
 }
 
 export { AvatarSize };
+export type { GraphNode, BaseApiResponse };
