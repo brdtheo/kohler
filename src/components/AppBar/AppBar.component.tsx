857c8daf75f4ddad657fec39b8bf2bf470a72cdc
Théo Billardey <toto.billardey@gmail.com>
2023-12-28 22:42:23 +0100

    feat(appbar): display channel description

--- src/components/AppBar/AppBar.component.tsx
+++ src/components/AppBar/AppBar.component.tsx
@@ -9,19 +9,30 @@ import Pin from '@icons/Pin';
 import { AppBarButton, AppBarSearch } from '.';
 
 type Props = {
+  /** The current channel's name */
   title: string;
+  /** The current channel's description */
+  subtitle?: string;
   onShowMembersList: () => void;
 };
 
-const AppBar: React.FC<Props> = ({ title, onShowMembersList }) => {
+const AppBar: React.FC<Props> = ({ title, subtitle, onShowMembersList }) => {
   return (
     <div className="bg-ebony p-2 border-b border-b-gunmetal shadow relative">
       <section className="flex items-center h-8">
-        <div className="flex items-center text-crestline select-none flex-1">
-          <div className="mx-2">
+        <div className="flex items-center text-crestline select-none flex-1 overflow-hidden">
+          <div className="mx-2 text-oslo">
             <Hashtag />
           </div>
           <h1 className="text-smoke font-medium gg-semibold">{title}</h1>
+          {!!subtitle && (
+            <>
+              <div className="h-6 w-px bg-crestline bg-opacity-10 mx-4 text-sm" />
+              <span className="text-crestline w-full text-sm  whitespace-nowrap text-ellipsis overflow-hidden gg-medium leading-[18px]">
+                {subtitle}
+              </span>
+            </>
+          )}
         </div>
 
         <div className="flex items-center text-crestline">
