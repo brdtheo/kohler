929fc8aef838fc8c4534460ee7dcccf67c175287
Théo Billardey <toto.billardey@gmail.com>
2023-12-28 19:55:54 +0100

    feat(channel): create channel api

---
+++ src/libs/channel/api.ts
@@ -0,0 +1,45 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query';
+import { gql } from 'graphql-request';
+
+import { BaseApiResponse } from '@api/types';
+
+import getApiRequestHeaders from '@utils/getApiRequestHeaders';
+import getCleanGraphQLResponse from '@utils/getCleanGraphQLResponse';
+
+import { Channel } from '@libs/channel/types';
+
+// Define a service using a base URL and expected endpoints
+export const channelApi = createApi({
+  reducerPath: 'channelApi',
+  baseQuery: graphqlRequestBaseQuery({
+    url: import.meta.env.VITE_BASE_API_URL,
+    prepareHeaders: (headers) => getApiRequestHeaders(headers),
+  }),
+  endpoints: (builder) => ({
+    getChannelList: builder.query<Channel[], number>({
+      query: (serverId: number) => ({
+        document: gql`
+          query getChannelList($page: Int = 1, $per_page: Int = 30) {
+            channelsCollection(filter: { server_id: { eq: ${serverId} } }, orderBy: { created_at: AscNullsLast }) {
+              edges {
+                node {
+                  id
+                  name
+                  description
+                  type
+                }
+              }
+            }
+          }
+        `,
+      }),
+      transformResponse: (response: BaseApiResponse<Channel>) =>
+        getCleanGraphQLResponse(response),
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetChannelListQuery } = channelApi;
