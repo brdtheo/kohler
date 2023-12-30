1c179de88a4fc95024988a42429bc2942abc877c
Théo Billardey <toto.billardey@gmail.com>
2023-12-30 17:41:27 +0100

    feat(message): update SentMesage type

--- src/libs/message/types.ts
+++ src/libs/message/types.ts
@@ -16,16 +16,7 @@ export type Message = {
   sent_at: string;
 };
 
-export type SentMessage = Pick<User, 'thumbnail' | 'username'> &
-  Pick<Member, 'display_name'> & {
-    /** Message ID */
-    id: number;
-    /** Content of the Message: text can be used as well as images files */
-    content: string;
-    /** Type of the messagge of the Message: text can be used as well as images files */
-    type: `${MessageType}`;
-    /** ID of the User who sent the Message */
-    author: number;
-    /** Date when the Member sent the Message */
-    sent_at: string;
-  };
+export type SentMessage = Message & {
+  users: Pick<User, 'id' | 'thumbnail' | 'username'>;
+  members: Pick<Member, 'display_name'>;
+};
