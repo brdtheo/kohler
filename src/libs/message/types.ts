5ec7862d635f316206221981074608fffe1fe83d
Théo Billardey <toto.billardey@gmail.com>
2023-12-30 18:50:15 +0100

    feat(message): update Message type and add MessageInput

--- src/libs/message/types.ts
+++ src/libs/message/types.ts
@@ -5,16 +5,20 @@ import { MessageType } from './constants';
 
 export type Message = {
   /** Message ID */
-  id: number;
+  id: string;
   /** Content of the Message: text can be used as well as images files */
   content: string;
   /** Type of the messagge of the Message: text can be used as well as images files */
   type: `${MessageType}`;
   /** ID of the User who sent the Message */
-  author: number;
+  author_id: string;
+  /** ID of the channel the message has been sent */
+  channel_id: string;
   /** Date when the Member sent the Message */
   sent_at: string;
 };
+
+export type MessageInput = Omit<Message, 'id'>;
 
 export type SentMessage = Message & {
   users: Pick<User, 'id' | 'thumbnail' | 'username'>;
