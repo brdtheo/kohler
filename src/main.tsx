c5981fd6ce136f02ec73ebb8bb2dc178e33c2e43
Théo Billardey <toto.billardey@gmail.com>
2024-04-28 21:19:11 +0200

    feat(redux): persist auth reducer
    Temporarily persist auth reducer for dev purposes.
    
    Unsafe part to be replaced later on with IPC events/SafeStorage API.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import routes from '@routes';
 
-import { store } from '@store';
+import { persistor, store } from '@store';
 
 import RealtimeProvider from '@api/RealtimeProvider';
 
@@ -14,19 +15,21 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RealtimeProvider>
-        <HashRouter>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Routes>
-        </HashRouter>
-      </RealtimeProvider>
+      <PersistGate loading={<p>loading !!!!!!!</p>} persistor={persistor}>
+        <RealtimeProvider>
+          <HashRouter>
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+            </Routes>
+          </HashRouter>
+        </RealtimeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
